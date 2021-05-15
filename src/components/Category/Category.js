5cb3742ba397aca6493d87d6695234fe146c0ee7
Thomas Rognes <thomas_rog@hotmail.com>
2021-05-15 19:51:12 +0200

    Add functionality to change Category

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -2,15 +2,34 @@ import React, { useState } from "react";
 import { ScoreButton } from "../Scorebutton/ScoreButton";
 
 import "./Category.css";
+import { EditPen } from "../EditPen/EditPen";
 
 export function Category(props) {
   const { name } = props;
 
   const [categoryName, setCategoryName] = useState(name);
+  const [editing, setEditing] = useState(false);
 
   return (
     <div className="category">
-      <h2>{categoryName}</h2>
+      <div className="category-name">
+        {editing ? (
+          <input
+            value={categoryName}
+            onChange={(e) => setCategoryName(e.target.value)}
+            onfocusout={() => setEditing(false)}
+            className="category-name-input"
+          />
+        ) : (
+          <h2>{categoryName}</h2>
+        )}
+        <button
+          onClick={() => setEditing(!editing)}
+          className="remove-button-style"
+        >
+          <EditPen />
+        </button>
+      </div>
       <ScoreButton score={100} categoryName={categoryName} />
       <ScoreButton score={200} categoryName={categoryName} />
       <ScoreButton score={300} categoryName={categoryName} />
